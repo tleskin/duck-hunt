0e670883a2409100c180faeea7aa7976a1f0532d
Michelle Golle <michelleg54@gmail.com>
2015-07-21 14:24:09 -0600

    game keeps score and only allows three shots per duck

--- test/integration/player-shooting-test.js
+++ test/integration/player-shooting-test.js
@@ -10,8 +10,8 @@ describe('game', function () {
     const player = new Player;
     const game = new Game;
     var playerShot = player.shoot(2, 3);
-    game.hitIsAccurate(playerShot, duck);
-    assert.equal(game.hit, false);
+    game.scoreKeeper(playerShot, duck);
+    assert.equal(game.points, 0);
   });
 
   it('can hit', function () {
@@ -19,7 +19,38 @@ describe('game', function () {
     const player = new Player;
     const game = new Game;
     var playerShot = player.shoot(duck.x, duck.y);
-    game.hitIsAccurate(playerShot, duck);
-    assert.equal(game.hit, true);
+    game.scoreKeeper(playerShot, duck);
+    assert.equal(game.points, 1);
   });
+
+  it('can keep score', function() {
+    const duck = new Duck;
+    const player = new Player;
+    const game = new Game;
+    var playerShot1 = player.shoot(duck.x, duck.y);
+    game.scoreKeeper(playerShot1, duck);
+    var playerShot2 = player.shoot(duck.x, duck.y);
+    game.scoreKeeper(playerShot2, duck);
+    var playerShot3 = player.shoot(4, 8);
+    game.scoreKeeper(playerShot3, duck);
+    var playerShot4 = player.shoot(duck.x, duck.y);
+    game.scoreKeeper(playerShot4, duck);
+    assert.equal(game.points, 2);
+  });
+
+  it('can only shoot at one duck three times', function () {
+    const duck = new Duck;
+    const player = new Player;
+    const game = new Game;
+    var playerShot1 = player.shoot(1, 1);
+    game.scoreKeeper(playerShot1, duck);
+    var playerShot2 = player.shoot(1, 1);
+    game.scoreKeeper(playerShot2, duck);
+    var playerShot3 = player.shoot(1, 1);
+    game.scoreKeeper(playerShot3, duck);
+    var playerShot4 = player.shoot(duck.x, duck.y);
+    game.scoreKeeper(playerShot4, duck);
+    assert.equal(game.points, 0);
+  });
+
 });
