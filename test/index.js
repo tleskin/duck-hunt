a73e49b8c517ede28a757a3aff88ed093f417535
Thomas Leskin <tleskin@gmail.com>
2015-07-23 14:03:07 -0600

    Game is running, added images

--- test/index.js
+++ test/index.js
@@ -1,11 +1,10 @@
 const assert = require('chai').assert;
 require('./duck/duck-test.js');
-require('./player/player-test.js');
 require('./integration/player-shooting-test.js');
+require('./game/game-test.js');
 
 describe('Our test bundle', function () {
   it('should work', function () {
     assert(true);
   });
-
 });
