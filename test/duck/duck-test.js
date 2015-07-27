f601b94be3aba8691579a49c62891bdf3a6c67a7
Thomas Leskin <tleskin@gmail.com>
2015-07-26 22:22:37 -0600

    Fix duck test and associated code

--- test/duck/duck-test.js
+++ test/duck/duck-test.js
@@ -9,25 +9,33 @@ describe('Duck', function () {
 
   it('starts alive', function () {
     const duck = new Duck();
-    assert.equal(duck.alive, true );
+    assert.equal(duck.alive, true);
   });
 
   it('can die', function () {
     const duck = new Duck();
     duck.shot();
-    assert.equal(duck.alive, false );
+    assert.equal(duck.alive, false);
   });
 
   it('has a starting position and velocity', function () {
     const duck = new Duck();
-    assert.equal(duck.x, duck.x );
-    assert.equal(duck.y, 455 );
+    assert.equal(duck.x, duck.x);
+    assert.equal(duck.y, 455);
   });
 
   it('can move', function () {
-    // const duck = new Duck();
     const duck = new Duck(50, 50, { height:300, width: 400});
     duck.move();
-    assert.notEqual(duck.x, 50 );
+    assert.notEqual(duck.x, 50);
+  });
+
+  it('can falls to ground when shot', function () {
+    const duck = new Duck(50, 50, { height:300, width: 400});
+    assert.equal(duck.y, 455);
+    duck.shot();
+    assert.equal(duck.alive, false);
+    duck.move();
+    assert.equal(duck.y, 465);
   });
 });
